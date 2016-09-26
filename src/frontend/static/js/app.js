a3832af1b09851e70b9d37e04f08dc3759727470
enptfb55 <samato@Salvatores-MacBook-Pro.local>
2016-09-25 20:41:11 -0400

    added autocomplete to sidenav

--- src/frontend/static/js/app.js
+++ src/frontend/static/js/app.js
@@ -20,6 +20,28 @@ app.controller('AppCtrl', function($scope, $mdSidenav, $log){
 });
 
 app.controller('SideCtrl', function ($scope, $mdSidenav, $log, $http) {
+    var self = this;
+
+    self.selectedItemChange = function(text) {
+      //$log.info('Text changed to ' + text);
+    };
+
+    self.searchTextChange   = function(item) {
+      //$log.info('Item changed to ' + JSON.stringify(item));
+    };
+
+    self.querySearch = function(query) {
+      //$log.info('Query: ' + query);
+
+      //$log.info('Courses: ' + self.courses);
+      
+      var results = query ? self.courses.filter( createFilterFor(query) ) : self.courses,
+          deferred;
+
+      //$log.info('Results: ' + JSON.stringify(results));
+      return results;
+    };
+
     $scope.close = function () {
         // Component lookup should always be available since we are not using `ng-if`
         $mdSidenav('left').close()
@@ -28,6 +50,28 @@ app.controller('SideCtrl', function ($scope, $mdSidenav, $log, $http) {
                 });
     };
 
+    $http.get("get_courses").then(function (response) {
+        $log.info("loading get_courses");
+        self.courses = response.data.results.map(function(item) {
+            return {
+                value: item.toLowerCase(),
+                name: item
+            };
+        });
+    });
+
+
+     /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(item) {
+        return (item.value.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
 
 
 });
