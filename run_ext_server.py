7de62709d4f52404e602d3fbd68176a03f59c45f
Constantine Davantzis <c.davantzis@gmail.com>
2016-09-26 22:53:39 -0400

    Update terminology

--- run_ext_server.py
+++ run_ext_server.py
@@ -8,6 +8,6 @@ from config import ExternalConfig
 
 if __name__ == "__main__":
     app.config.from_object(ExternalConfig)
-    # IMPORTANT: DEBUG MUST NEVER BE TRUE ON EXTERNAL MACHINES!
+    # IMPORTANT: DEBUG MUST NEVER BE TRUE ON EXTERNALLY VISABLE SERVERS!
     app.run(host='0.0.0.0', port=80, debug=False)
 
