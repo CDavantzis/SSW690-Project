0b37e87bf642e09b89d2589afa467318f6de534a
Constantine Davantzis <c.davantzis@gmail.com>
2016-09-26 22:47:11 -0400

    refactor and comment "get_db"

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,9 +1,18 @@
-from flask import  g,current_app
+from flask import g, current_app
 from pymongo import MongoClient
+
+import catalog
+import schedule
 
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = MongoClient(current_app.config.get("MONGO_HOST"), port=current_app.config.get("MONGO_PORT"))
-    return db
+    """ Get MongoClient for current application
+    :returns New MongoClient if there is none in the current application context, otherwise existing MongoClient
+
+    Example:
+        from werkzeug.local import LocalProxy
+        db = LocalProxy(get_db)
+    """
+    if not hasattr(g, 'mongo_client'):
+        g.mongo_client = MongoClient(current_app.config.get("MONGO_HOST"), port=current_app.config.get("MONGO_PORT"))
+    return g.mongo_client
