feea298481d5e0235c7d122e84d4955696415b50
Constantine Davantzis <c.davantzis@gmail.com>
2016-09-27 20:09:18 -0400

    cleanup update script

--- src/app/db/catalog/__init__.py
+++ src/app/db/catalog/__init__.py
@@ -1,23 +1,6 @@
 import courses
 import degrees
 
-import os
-
-catalog_dir = os.path.dirname(os.path.relpath(__file__))
-
-
-def create(client): 
-    print 'Dropping catalog db...'
-    client.drop_database('catalog')
-
-    for file in os.listdir(catalog_dir):
-        file = os.path.join(catalog_dir, file)
-        if not file.endswith('.json'):
-            continue 
-
-        collection = os.path.basename(file[:file.find('.json')])
-        print 'Adding collection %s to db...' % collection
-        os.system('mongoimport --db catalog --collection ' + collection + ' --file ' + file + ' --jsonArray')
-
-
-
+if __name__ == "__main__":
+    courses.update_db()
+    degrees.update_db()
