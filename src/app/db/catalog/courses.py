681cab0f9315de17c76ba855aa5a58cf0782211a
Constantine Davantzis <c.davantzis@gmail.com>
2016-09-29 00:57:47 -0400

    show all duplicates on file error

--- src/app/db/catalog/courses.py
+++ src/app/db/catalog/courses.py
@@ -8,37 +8,33 @@ FILE_LOCATION = os.path.join(os.path.dirname(os.path.relpath(__file__)), FILE_NA
 COLLECTION_NAME = "courses"
 
 
-def check_for_duplicates(d):
-    h = {}
-    for a in d:
-        key = (a["letter"], a["number"])
-        if key in h:
-            return key
-        h[key] = 1
-    return False
-
-
 def load_data():
     """ Load Data From JSON File
 
     :return: List representation of local "courses" data
     :rtype: list
     """
-    return bson.json_util.loads(open(FILE_LOCATION).read())
+    data = bson.json_util.loads(open(FILE_LOCATION).read())
+
+    # Warn if there are duplicate courses
+    h = {}
+    for a in data:
+        key = (a["letter"], a["number"])
+        if key in h:
+            print RuntimeWarning("Duplicate course {0}-{1} in courses.json".format(*key))
+        h[key] = 1
+
+    return data
 
 
 def update_db():
     """ Update Database With Current JSON Data """
     db = mongo_client.catalog
-    new_data = load_data()
-    duplicate = check_for_duplicates(new_data)
-    if duplicate:
-        print RuntimeWarning("Duplicate course {0} in courses.json".format( duplicate))
     if COLLECTION_NAME not in db.collection_names():
-        db[COLLECTION_NAME].insert_many(new_data)
+        db[COLLECTION_NAME].insert_many(load_data())
     else:
         db.temp.drop()
-        db.temp.insert_many(new_data)
+        db.temp.insert_many(load_data())
         db.temp.aggregate([{"$out": COLLECTION_NAME}])
         db.temp.drop()
 
