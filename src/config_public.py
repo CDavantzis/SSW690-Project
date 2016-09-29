d982966b36a583a32195175a2f5714f539cda67a
Constantine Davantzis <c.davantzis@gmail.com>
2016-09-29 00:11:40 -0400

    public and private config files

---
+++ src/config_public.py
@@ -0,0 +1,12 @@
+class BaseConfig(object):
+
+    # Flask
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 megabytes
+
+    # MongoDB
+    MONGO_HOST = '127.0.0.1'
+    MONGO_PORT = 27017
+
+
+
+
--- src/run.py
+++ src/run.py
@@ -1,14 +1,18 @@
 """ Run Web Server """
-from app import flask_app
-import config
-import argparse
 import sys
+import argparse
+
+from app import flask_app
+
+try:
+    import config_private as config
+except ImportError:
+    import config_public as config
 
 
 def local_server():
-    """ Launch local server
-    """
-    flask_app.config.from_object(config.DevConfig)
+    """ Launch local server """
+    flask_app.config.from_object(config.BaseConfig)
     flask_app.run(host="127.0.0.1", port=5000, threaded=True, debug=True)
     sys.exit(1)
 
@@ -20,7 +24,7 @@ def public_server():
         See http://flask.pocoo.org/docs/0.11/deploying/#deployment
 
     """
-    flask_app.config.from_object(config.ExtConfig)
+    flask_app.config.from_object(config.BaseConfig)
     # IMPORTANT: DEBUG MUST NEVER BE TRUE ON EXTERNALLY VISIBLE SERVERS!
     flask_app.run(host='0.0.0.0', port=80, threaded=True, debug=False)
     sys.exit(1)
