30f49f93518fd49b6f79d31dd7c27daf525e76fa
Constantine Davantzis <c.davantzis@gmail.com>
2016-10-17 01:20:04 -0400

    Isolate Demos
    Allow demos to work indepedantly of other api endpoints

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, jsonify
 from werkzeug.local import LocalProxy
 from context import get_db
 
-from demos import demos
 
 flask_app = Flask(__name__)
 mongo_client = LocalProxy(get_db)
+
+from demos import demos
 flask_app.register_blueprint(demos)
 
 
@@ -20,26 +21,15 @@ def get_courses():
     return jsonify(results=list(c))
 
 
-@flask_app.route('/api/tree/courses')
+@flask_app.route('/api/tree/courses_v2')
 def get_course_tree():
     c = mongo_client.catalog.courses.aggregate([{"$sort": {"number": 1}},
                                                 {"$group": {"_id": "$letter",
                                                             "nodes": {"$push": {
-                                                                "title": {"$concat": ["$letter", "-",
-                                                                                      "$number", " ",
-                                                                                      "$name"]}}}}},
-                                                {"$sort": {"_id": 1}},
-                                                {"$project": {"_id": 0, "title": "$_id", "nodes": "$nodes"}}])
-    return jsonify(results=list(c))
-
-
-@flask_app.route('/api/tree/courses_v2')
-def get_course_tree_v2():
-    c = mongo_client.catalog.courses.aggregate([{"$sort": {"number": 1}},
-                                                {"$group": {"_id": "$letter",
-                                                            "nodes": {"$push": {
-                                                                "a_attr": {"data-letter":"$letter", "data-number": "$number"},
-                                                                "text": {"$concat": ["$letter", "-", "$number", " ",
+                                                                "a_attr": {"data-letter": "$letter",
+                                                                           "data-number": "$number"},
+                                                                "text": {"$concat": ["$letter", "-",
+                                                                                     "$number", " ",
                                                                                      "$name"]}}}}},
                                                 {"$sort": {"_id": 1}},
                                                 {"$project": {"_id": 0, "text": "$_id", "children": "$nodes"}}])
--- src/app/demos.py
+++ src/app/demos.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, jsonify
+from app import mongo_client
 
-
-demos = Blueprint('simple_page', __name__, template_folder='templates/demos', url_prefix='/demos')
+demos = Blueprint('demos', __name__, template_folder='templates/demos', url_prefix='/demos')
 
 
 @demos.route('/angular_ui_tree')
@@ -9,7 +9,34 @@ def angular_ui_tree():
     return render_template('angular_ui_tree.html')
 
 
+@demos.route('/angular_ui_tree/js')
+def angular_ui_tree_js():
+    c = mongo_client.catalog.courses.aggregate([{"$sort": {"number": 1}},
+                                                {"$group": {"_id": "$letter",
+                                                            "nodes": {"$push": {
+                                                                "title": {"$concat": ["$letter", "-",
+                                                                                      "$number", " ",
+                                                                                      "$name"]}}}}},
+                                                {"$sort": {"_id": 1}},
+                                                {"$project": {"_id": 0, "title": "$_id", "nodes": "$nodes"}}])
+    return jsonify(results=list(c))
+
+
 @demos.route('/js_tree')
 def js_tree():
     return render_template('js_tree.html')
 
+
+@demos.route('/js_tree/js')
+def js_tree_js():
+    c = mongo_client.catalog.courses.aggregate([{"$sort": {"number": 1}},
+                                                {"$group": {"_id": "$letter",
+                                                            "nodes": {"$push": {
+                                                                "a_attr": {"data-letter": "$letter",
+                                                                           "data-number": "$number"},
+                                                                "text": {"$concat": ["$letter", "-",
+                                                                                     "$number", " ",
+                                                                                     "$name"]}}}}},
+                                                {"$sort": {"_id": 1}},
+                                                {"$project": {"_id": 0, "text": "$_id", "children": "$nodes"}}])
+    return jsonify(results=list(c))
