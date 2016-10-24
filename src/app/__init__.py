d271521573a0188f4ec010f991cbf8aa4add3ea8
Constantine Davantzis <c.davantzis@gmail.com>
2016-10-24 16:57:29 -0400

    Map out required routes
    Map out routes required for site to function

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, abort
 from werkzeug.local import LocalProxy
 from context import get_db
 
@@ -18,17 +18,41 @@ def home():
 
 @flask_app.route('/api/courses/list')
 def get_courses():
-    """
-
-    :return:
-    """
+    """ List all courses in database """
     return jsonify(results=list(db.catalog.courses.get_all()))
 
 
 @flask_app.route('/api/courses/tree')
 def get_course_tree():
-    """
-
-    :return:
-    """
+    """ List all courses in database in a tree """
     return jsonify(results=list(db.catalog.courses.get_tree()))
+
+
+@flask_app.route('/api/courses/info')
+def get_course_info():
+    """ Get the course info for a specified course """
+    abort(501)
+
+
+@flask_app.route('/api/schedule/semesters')
+def get_scheduled_semesters():
+    """ List all semesters available in schedule database """
+    abort(501)
+
+
+@flask_app.route('/api/schedule/list')
+def get_scheduled_courses():
+    """ List all courses in database for a specified semester """
+    abort(501)
+
+
+@flask_app.route('/api/schedule/tree')
+def get_scheduled_course_tree():
+    """ List all courses in database for a specified semester in a tree """
+    abort(501)
+
+
+@flask_app.route('/api/schedule/combinations')
+def get_scheduled_course_combinations():
+    """ Get the course info for specified courses """
+    abort(501)
