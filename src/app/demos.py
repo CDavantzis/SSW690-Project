314c6c78334d4a8cb41591c700d62a7e62d40d36
Constantine Davantzis <c.davantzis@gmail.com>
2016-10-25 21:16:55 -0400

    Seperate jsTree Demo Routes for courses and schedule
    Courses jsTree Demo Path:
    /demos/js_tree/courses
    
    Schedule jsTree Demo Path:
    /demos/js_tree/schedule

--- src/app/demos.py
+++ src/app/demos.py
@@ -3,23 +3,14 @@ from app import mongo_client
 
 demos = Blueprint('demos', __name__, template_folder='templates/demos', url_prefix='/demos')
 
-@demos.route('/js_tree')
-def js_tree():
-    return render_template('js_tree.html')
+
+@demos.route('/js_tree/courses')
+def js_tree_courses():
+    """ Courses jsTree Demo """
+    return render_template('/js_tree/courses.html')
 
 
-@demos.route('/js_tree/js')
-def js_tree_js():
-    c = mongo_client.catalog.courses.aggregate([{"$sort": {"number": 1}},
-                                                {"$group": {"_id": "$letter",
-                                                            "nodes": {"$push": {
-                                                                "a_attr": {"data-letter": "$letter",
-                                                                           "data-number": "$number"},
-                                                                "text": {"$concat": ["$letter", "-",
-                                                                                     "$number", " ",
-                                                                                     "$name"]}}}}},
-                                                {"$sort": {"_id": 1}},
-                                                {"$project": {"_id": 0, "text": "$_id", "children": "$nodes"}}])
-    return jsonify(results=list(c))
-
-
+@demos.route('/js_tree/schedule')
+def js_tree_schedule():
+    """ Schedule jsTree Demo """
+    return render_template('/js_tree/schedule.html')
