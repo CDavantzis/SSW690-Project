b1abfb88ef49d1bab848a40344679579bf0c43eb
Constantine Davantzis <c.davantzis@gmail.com>
2016-10-25 20:47:16 -0400

    Invalidate static file cache on file update
    Source:
    https://gist.github.com/Ostrovski/f16779933ceee3a9d181

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify, abort
+import os
+
+from flask import Flask, render_template, jsonify, abort, request
 from werkzeug.local import LocalProxy
 from context import get_db
-
 
 flask_app = Flask(__name__)
 mongo_client = LocalProxy(get_db)
@@ -9,6 +10,37 @@ mongo_client = LocalProxy(get_db)
 import db
 from demos import demos
 flask_app.register_blueprint(demos)
+
+
+@flask_app.url_defaults
+def hashed_static_file_url(endpoint, values):
+    """
+
+
+    :param endpoint:
+    :param values:
+    :return:
+
+    source:
+    https://gist.github.com/Ostrovski/f16779933ceee3a9d181
+
+    """
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
+        if filename:
+            static_folder = flask_app.static_folder
+            blueprint = endpoint.rsplit('.', 1)[0] if '.' in endpoint else request.blueprint
+            if blueprint and flask_app.blueprints[blueprint].static_folder:
+                static_folder = flask_app.blueprints[blueprint].static_folder
+            filepath = os.path.join(static_folder, filename)
+            if os.path.exists(filepath):
+                values['_'] = int(os.stat(filepath).st_mtime)
+
+
+def static_file_hash(filename):
+    return int(os.stat(filename).st_mtime)
+
+
 
 
 @flask_app.route('/')
